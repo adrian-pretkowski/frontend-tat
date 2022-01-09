c62eb4945e52bb6c08d431c15ac8cd567174eba2
Adi <adrian.pretkowski@gmail.com>
2022-01-09 22:01:12 +0100

    changed api url

--- src/static/apiServices.js
+++ src/static/apiServices.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080/api/';
+// export const API_BASE_URL = 'http://localhost:8080/api/';
+export const API_BASE_URL = 'https://test-tat-backend.herokuapp.com/';
 
 export const API_LOGIN = API_BASE_URL + 'login';
 export const API_REFRESH_TOKEN = API_BASE_URL + 'token/refresh';
